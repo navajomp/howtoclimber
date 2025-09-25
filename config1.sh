69ffd290132b6a128f47d958385f05f56b7bea5e
Navajyoth M P <navajyothmp@Navajyoths-iMac.local>
2025-09-25 15:24:00 +0900

    readme changed

--- config1.sh
+++ config1.sh
@@ -1,24 +1,36 @@
-### Compiler #####################
-FC = $(CONDA_PREFIX)/bin/gfortran
+# Detect operating system
+UNAME_S := $(shell uname -s)
 
-### Library locations ############
-NC_CROOT=$(CONDA_PREFIX)
-NC_FROOT=$(CONDA_PREFIX)
-INC_NC  = -I$(NC_FROOT)/include
-LIB_NC  = -L$(NC_FROOT)/lib -lnetcdff -L$(NC_CROOT)/lib -lnetcdf
+ifeq ($(UNAME_S),Darwin)
+    # macOS configuration
+    ### Compiler #####################
+    FC = gfortran-15
 
-### Fortran flags ################
+    ### Library locations ############
+    NC_CROOT = /opt/homebrew/opt/netcdf
+    NC_FROOT = /opt/homebrew/opt/netcdf-fortran
+    INC_NC  = -I$(NC_FROOT)/include -I$(NC_CROOT)/include
+    LIB_NC  = -L$(NC_FROOT)/lib -lnetcdff -L$(NC_CROOT)/lib -lnetcdf
+else
+    # Linux configuration
+    ### Compiler #####################
+    FC = gfortran
+
+    ### Library locations ############
+    NC_CROOT = /usr
+    NC_FROOT = /usr
+    INC_NC  = -I$(NC_FROOT)/include
+    LIB_NC  = -L$(NC_FROOT)/lib -lnetcdff -L$(NC_CROOT)/lib -lnetcdf
+endif
+
 FFLAGS  = -ffree-line-length-none -I$(objdir) -J$(objdir)
 
 ifeq ($(openmp), 1)
     FFLAGS  = -I$(objdir) -J$(objdir) -ffree-line-length-none -fomit-frame-pointer -fopenmp
 endif
 
-### Linking flags ################
 LFLAGS  = $(LIB_NC)
 
-### Debug / optimization flags ###
-DFLAGS_NODEBUG = -O2
+DFLAGS_NODEBUG = -O2 -march=native
 DFLAGS_DEBUG   = -w -g -ggdb -ffpe-trap=invalid,zero,overflow,underflow -fbacktrace -fcheck=all
 DFLAGS_PROFILE = -O2 -pg
-
